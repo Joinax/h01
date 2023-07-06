cd14fc5f0ada4c05c050ee1fb20b5b59373244b2
Joinax <fintroll18@gmail.com>
2023-07-06 22:26:13 +0300

    add Resolution

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ app.post('/videos', (req: Request, res: Response) => {
             "field": "author"
         })
     }
-    if(availableResolutions && !availableResolutions.every(r => Object.keys(Resolutions).includes(r))){
+    if(availableResolutions && !availableResolutions.every(r => Object.keys(Resolutions).includes(r)) || typeof availableResolutions !=='string'){
         apiErrorResult.push({
             "message": "string",
             "field": "availableResolutions"
